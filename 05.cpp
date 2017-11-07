3be7fb25c0a7ee1e1163868435cc70e46f88dfa3
noviaretno <32433423+noviaretno@users.noreply.github.com>
2017-11-07 08:07:23 -0800

    hasil tinggi dan berat badan

--- 05.cpp
+++ 05.cpp
@@ -10,4 +10,15 @@ int main () {
  
  cout << "Masukkan berat badan dalam kg : ";
  cin >> berat; 
+ 
+ if (berat<tinggi/2.5) {
+  cout << "Hasil : Kurus " << endl;
+ }
+ else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
+  cout <<  "Hasil : Normal" << endl; 
+ }
+ 
+ else if (tinggi/2.3 < berat)
+  cout << "Hasil : Gemuk" << endl;
+ 
 }
